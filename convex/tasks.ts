c7e83837de8aa99ea89a6b8c18292c9875f1bb38
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2023-10-26 20:54:30 -0700

    createTask

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -1,8 +1,18 @@
-import { query } from './_generated/server';
+import { mutation, query } from './_generated/server'
+import { v } from 'convex/values'
 
 export const get = query({
-	args: {},
-	handler: async (ctx) => {
-		return await ctx.db.query('tasks').collect();
-	}
-});
+  args: {},
+  handler: async (ctx) => {
+    return await ctx.db.query('tasks').collect()
+  }
+})
+
+// Create a new task with the given text
+export const createTask = mutation({
+  args: { text: v.string() },
+  handler: async (ctx, args) => {
+    const newTaskId = await ctx.db.insert('tasks', { text: args.text })
+    return newTaskId
+  }
+})
